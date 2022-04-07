5438e1d839ded52551e9d0ac0acefd3be7e392a7
john <john.davis.2015.03@gmail.com>
2022-04-06 21:36:59 -0500

    Tip: Static Generation vs Server-side Rendering

---
+++ pages/about.jsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+export default function AboutPage() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>About - Page</title>
+        <meta name="description" content="About Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Ir a
+          <Link href="/">
+            <a> Home</a>
+          </Link>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{" "}
+          <code className={styles.code}>pages/about.jsx</code>
+        </p>
+      </main>
+    </div>
+  );
+}
