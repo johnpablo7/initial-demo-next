f6c910f77d5b252a3b0bd5c836441aa065a5e31d
john <john.davis.2015.03@gmail.com>
2022-04-07 22:14:35 -0500

    Fin de la sección 2

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,29 +1,27 @@
-import Head from "next/head";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import { DarkLayout } from "../components/layouts/DarkLayout";
+import { MainLayout } from "../components/layouts/MainLayout";
 
 export default function AboutPage() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>About - Page</title>
-        <meta name="description" content="About Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <h1>About Page</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Ir a
-          <Link href="/">
-            <a> Home</a>
-          </Link>
-        </h1>
+      <h1 className={"title"}>
+        Ir a<Link href="/"> Home</Link>
+      </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/about.jsx</code>
-        </p>
-      </main>
-    </div>
+      <p className={"description"}>
+        Get started by editing <code className={"code"}>pages/about.jsx</code>
+      </p>
+    </>
   );
 }
+
+AboutPage.getLayout = function getLayout(page) {
+  return (
+    <MainLayout>
+      <DarkLayout>{page}</DarkLayout>
+    </MainLayout>
+  );
+};
